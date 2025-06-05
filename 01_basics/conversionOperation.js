341953bdda3159d09982fc8c7aa560724d32ed84
Priyanshukum4r <138358895+Priyanshukum4r@users.noreply.github.com>
2025-06-05 19:56:22 +0000

    conversionOperation

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,81 @@
+let score =133
+
+console.log(typeof score);   //returns a number
+score="133"
+console.log(typeof (score));  // returns a String ,this is another way to print the type of data
+
+let valueInNumber=Number(score)  //Number here is type of or a kind of class and here it is some type casting
+console.log(typeof valueInNumber);
+
+let  num="33abc"
+console.log(typeof num);//returns String
+let vum=Number(num)
+console.log(typeof vum);// returns number,but actual value of vum is NAN
+console.log(vum);
+
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+
+
+
+
+
