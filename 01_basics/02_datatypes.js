1f89141f3e98c0c0d9683d56c89e09d6cd25b72b
Priyanshukum4r <138358895+Priyanshukum4r@users.noreply.github.com>
2025-06-05 19:33:46 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict";  //treat all JS code as newer Version
+
+//alert(3+3)   //will give error as we are using nodejs not browser
+let name="priyansu"
+let age=23
+let isLoggedIn=false
+
+
+//number ==> (2 to power 53)-1 for both -ve and +ve
+//bigint
+//String ==> " ", ''
+//boolean ==> true/false
+//null ==> standalone value
+//undefined
+//symbol ==>Uniqueness
+
+//object
+
+//type of 
+console.log(typeof "Priyanshu");
+console.log(typeof age);
+console.log(typeof null);  //object
+console.log(typeof undefined);  //undefined
+
+
+
